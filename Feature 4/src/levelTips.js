5c75316d508c7657d41d971b8dc44f6fa8c5f5b0
JackDalton3110 <c00205943@itcarlow.ie>
2019-01-22 09:52:34 +0000

    Tips drawing

--- Feature 4/src/levelTips.js
+++ Feature 4/src/levelTips.js
@@ -12,8 +12,21 @@ class levelTips
     canvas.style.top = this.tipsImg.offsetTop;
     gameNs.display = true;
     gameNs.wall = true
+    gameNs.tipsCount = 0
     gameNs.breakwall = false
     gameNs.goalwall = false;
+    gameNs.tipsArr =[
+        {
+          "Tips":"Walls can not be blown up by the bomb"
+        },
+        {
+          "Tips":"Broken walls can be blown up by the bomb, this can create a new passage"
+        },
+        {
+          "Tips":"The player aims to find the goal which is hidden underneath a broken wall"
+        },
+      ]
+    gameNs.tips = gameNs.tipsArr[gameNs.tipsCount].Tips
   }
 
   update()
@@ -27,20 +40,28 @@ class levelTips
   {
     var canvas = document.getElementById('gameCanvas')
     var ctx = canvas.getContext('2d')
+
+
     if(gameNs.wall === true)
     {
       ctx.clearRect(0,0,canvas.width, canvas.height)
       this.tipsImg.src = "../Images/wall.png"
+      gameNs.Tips = gameNs.tipsArr[gameNs.tipsCount].Tips
+
     }
     else if(gameNs.breakwall === true)
     {
       ctx.clearRect(0,0,canvas.width, canvas.height)
       this.tipsImg.src = "../Images/breakwall.png"
+      gameNs.tipsCount+=1
+      gameNs.Tips = gameNs.tipsArr[gameNs.tipsCount].Tips
     }
     else if(gameNs.goalwall === true)
     {
     ctx.clearRect(0,0,canvas.width, canvas.height)
       this.tipsImg.src = "../Images/goal.png"
+      gameNs.tipsCount+=1
+      gameNs.Tips = gameNs.tipsArr[gameNs.tipsCount].Tips
     }
     else if(gameNs.display === false)
     {
@@ -57,7 +78,9 @@ class levelTips
     if(gameNs.display === true)
     {
       var image = this.tipsImg
-
+      ctx.font = '38px Impact';
+      ctx.fillStyle = "Black"
+      ctx.fillText(gameNs.Tips, 300,650)
       ctx.drawImage(image,400,400,200,200)
     }
   }
