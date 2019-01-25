2e654e73c1c3bf9560e3c78a5652cfed74f10c89
JackDalton3110 <c00205943@itcarlow.ie>
2019-01-25 17:20:34 +0000

    updates

--- Feature 4/src/levelTips.js
+++ Feature 4/src/levelTips.js
@@ -4,18 +4,21 @@ class levelTips
   constructor()
   {
     this.tipsImg = new Image()
-    var canvas = document.getElementById("gameCanvas")
+    var canvas = document.getElementById("mycanvas")
+    var tips = ""
+    gameNs.tips = tips
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     canvas.style.position = "absolute";
     canvas.style.left = this.tipsImg.offsetLeft;
     canvas.style.top = this.tipsImg.offsetTop;
     gameNs.display = true;
-    gameNs.wall = true
-    gameNs.tipsCount = 0
-    gameNs.breakwall = false
-    gameNs.goalwall = false;
-    gameNs.tipsArr =[
+    this.wall = true
+    this.breakwall = false
+    this.goalwall = false;
+    this.spacePressed = false
+    this.tipsCount = 0
+    this.tipsArr =[
         {
           "Tips":"Walls can not be blown up by the bomb"
         },
@@ -25,46 +28,55 @@ class levelTips
         {
           "Tips":"The player aims to find the goal which is hidden underneath a broken wall"
         },
+        {
+          "Tips":"Pressing the Space button will place a bomb"
+        }
       ]
-    gameNs.tips = gameNs.tipsArr[gameNs.tipsCount].Tips
+    this.tips = this.tipsArr[this.tipsCount].Tips
+    this.Leveltipdisplay()
   }
 
   update()
   {
-    var canvas = document.getElementById('gameCanvas')
+    var canvas = document.getElementById('mycanvas')
     var ctx = canvas.getContext('2d')
-    this.Leveltipdisplay()
+    //this.Leveltipdisplay()
   }
 
   Leveltipdisplay()
   {
-    var canvas = document.getElementById('gameCanvas')
+    var canvas = document.getElementById('mycanvas')
     var ctx = canvas.getContext('2d')
 
 
-    if(gameNs.wall === true)
+    if(this.wall === true)
     {
       ctx.clearRect(0,0,canvas.width, canvas.height)
-      this.tipsImg.src = "../Images/wall.png"
-      gameNs.Tips = gameNs.tipsArr[gameNs.tipsCount].Tips
+      this.tipsImg.src = "img/borderWallDown.png"
+      this.tips = (this.tipsArr[this.tipsCount].Tips)
 
     }
-    else if(gameNs.breakwall === true)
+    else if(this.breakwall === true)
     {
       ctx.clearRect(0,0,canvas.width, canvas.height)
-      this.tipsImg.src = "../Images/breakwall.png"
-      gameNs.tipsCount+=1
-      gameNs.Tips = gameNs.tipsArr[gameNs.tipsCount].Tips
+      this.tipsImg.src = "img/destructableWall.png"
+      this.tipsCount +=1
+      this.tips = (this.tipsArr[this.tipsCount].Tips)
     }
-    else if(gameNs.goalwall === true)
+    else if(this.goalwall === true)
     {
     ctx.clearRect(0,0,canvas.width, canvas.height)
-      this.tipsImg.src = "../Images/goal.png"
-      gameNs.tipsCount+=1
-      gameNs.Tips = gameNs.tipsArr[gameNs.tipsCount].Tips
+      this.tipsImg.src = "img/exit.png"
+      this.tipsCount +=1
+      this.tips = (this.tipsArr[this.tipsCount].Tips)
     }
-    else if(gameNs.display === false)
+    else if(gameNs.display === false && this.tipsCount < 3)
     {
+      ctx.clearRect(0,0,canvas.width, canvas.height)
+      this.tipsCount+=1
+      this.tips = (this.tipsArr[this.tipsCount].Tips)
+    }
+    else if (this.spacePressed === true){
       ctx.clearRect(0,0,canvas.width, canvas.height)
     }
   }
@@ -72,16 +84,19 @@ class levelTips
 
   render()
   {
-    var canvas = document.getElementById('gameCanvas')
+    var canvas = document.getElementById('mycanvas')
     var ctx = canvas.getContext('2d')
     document.body.style.background = "#FFFACD"
     if(gameNs.display === true)
     {
       var image = this.tipsImg
-      ctx.font = '38px Impact';
+      ctx.font = '38px Adventure';
       ctx.fillStyle = "Black"
-      ctx.fillText(gameNs.Tips, 300,650)
-      ctx.drawImage(image,400,400,200,200)
+      ctx.fillText(this.tips, 100,900)
+      ctx.drawImage(image,900,400,200,200)
+    }
+    else if (gameNs.display===false && this.spacePressed === false) {
+      ctx.fillText(this.tips, 100,900)
     }
   }
 }
